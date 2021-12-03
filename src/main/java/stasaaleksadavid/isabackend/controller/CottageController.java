0a26c5ad73ae335233e3af81bc08f8db0d19872c
nastasjadostanic <64745304+davidegelja@users.noreply.github.com>
2021-12-03 19:17:15 +0100

    Added CottageOwner, CottageAppointment, CottageAdditionalServices

--- src/main/java/stasaaleksadavid/isabackend/controller/CottageController.java
+++ src/main/java/stasaaleksadavid/isabackend/controller/CottageController.java
@@ -1,13 +1,15 @@
 package stasaaleksadavid.isabackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import stasaaleksadavid.isabackend.exception.ResourceNotFoundException;
 import stasaaleksadavid.isabackend.model.Cottage;
 import stasaaleksadavid.isabackend.repository.CottageRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -19,4 +21,54 @@ public class CottageController {
     //get all
     @GetMapping("/cottages")
     public List<Cottage> getAllCottages(){return cottageRepository.findAll();}
+
+    //create
+    @PostMapping("/cottages")
+    public Cottage createCottage(@RequestBody Cottage cottage){
+        return cottageRepository.save(cottage);
+    }
+
+    //get by id
+    @GetMapping("/cottages/{id}")
+    public ResponseEntity<Cottage> getCottageById(@PathVariable Long id){
+        Cottage cottage = cottageRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cottage does not exist with id:"+ id));
+        return ResponseEntity.ok(cottage);
+    }
+
+    //get by owner id
+
+   // @GetMapping("/cottages/owner/{ownerid}")
+   // public List<Cottage> getCottageByOwnerId(@PathVariable Long ownerid){
+   //     return cottageRepository.findByOwnerId(ownerid);
+  //  }
+
+    //update
+    @PutMapping("/cottages/{id}")
+    public ResponseEntity<Cottage> updateCottage(@PathVariable Long id,@RequestBody Cottage cottageDetails){
+        Cottage cottage = cottageRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cottage does not exist with id:"+ id));
+
+        cottage.setName(cottageDetails.getName());
+        cottage.setAddress(cottageDetails.getAddress());
+        cottage.setDescription(cottageDetails.getAddress());
+        cottage.setRating(cottageDetails.getRating());
+        cottage.setNumberOfRooms(cottageDetails.getNumberOfRooms());
+        cottage.setOwnerId(cottageDetails.getOwnerId());
+        cottage.setRules(cottageDetails.getRules());
+
+        Cottage updatedCottage = cottageRepository.save(cottage);
+        return ResponseEntity.ok(updatedCottage);
+    }
+
+
+    //delete
+    @DeleteMapping("/cottages/{id}")
+    public Map<String, Boolean> deleteCottage(@PathVariable Long id){
+
+        Cottage cottage = cottageRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cottage does not exist with id:"+ id));
+
+        cottageRepository.delete(cottage);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return (Map<String, Boolean>) ResponseEntity.ok(response);
+    }
 }
--- src/main/java/stasaaleksadavid/isabackend/controller/CottageOwnerController.java
+++ src/main/java/stasaaleksadavid/isabackend/controller/CottageOwnerController.java
@@ -1,10 +1,13 @@
 package stasaaleksadavid.isabackend.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import stasaaleksadavid.isabackend.exception.ResourceNotFoundException;
 import stasaaleksadavid.isabackend.model.CottageOwner;
 import stasaaleksadavid.isabackend.repository.CottageOwnerRepository;
 
@@ -19,4 +22,50 @@ public class CottageOwnerController {
 
     @GetMapping("/cottageowners")
     public List<CottageOwner> getAllCottageOwners(){return cottageOwnerRepository.findAll();}
+
+
+    //create
+    @PostMapping("/cottageowners")
+    public CottageOwner createCottageOwner(@RequestBody CottageOwner cottageOwner){
+        return cottageOwnerRepository.save(cottageOwner);
+    }
+
+    //get by id
+    @GetMapping("/cottageowners/{id}")
+    public ResponseEntity<CottageOwner> getCottageOwnerById(@PathVariable Long id){
+        CottageOwner cottageOwner = cottageOwnerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("CottageOwner does not exist with id:"+ id));
+        return ResponseEntity.ok(cottageOwner);
+    }
+
+    //update
+    @PutMapping("/cottageowners/{id}")
+    public ResponseEntity<CottageOwner> updateCottageOwner(@PathVariable Long id,@RequestBody CottageOwner cottageOwnerDetails){
+        CottageOwner cottageOwner = cottageOwnerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("CottageOwner does not exist with id:"+ id));
+
+        cottageOwner.setPassword(cottageOwnerDetails.getPassword());
+        cottageOwner.setFirstName(cottageOwnerDetails.getFirstName());
+        cottageOwner.setLastName(cottageOwnerDetails.getLastName());
+        cottageOwner.setDateOfBirth(cottageOwnerDetails.getDateOfBirth());
+        cottageOwner.setEmail(cottageOwnerDetails.getEmail());
+        cottageOwner.setPhoneNumber(cottageOwnerDetails.getPhoneNumber());
+        cottageOwner.setAddress(cottageOwnerDetails.getAddress());
+        cottageOwner.setCity(cottageOwnerDetails.getCity());
+        cottageOwner.setCountry(cottageOwnerDetails.getCountry());
+
+        CottageOwner updatedCottageOwner = cottageOwnerRepository.save(cottageOwner);
+        return ResponseEntity.ok(updatedCottageOwner);
+    }
+
+
+    //delete
+    @DeleteMapping("/cottageowners/{id}")
+    public Map<String, Boolean> deleteCottageOwner(@PathVariable Long id){
+
+        CottageOwner cottageOwner = cottageOwnerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("CottageOwner does not exist with id:"+ id));
+
+        cottageOwnerRepository.delete(cottageOwner);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return (Map<String, Boolean>) ResponseEntity.ok(response);
+    }
 }
