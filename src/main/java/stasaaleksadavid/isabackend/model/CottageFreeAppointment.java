ce33d9c1ae0d059e7ccf0b204bbbdef4a77bc1dd
nastasjadostanic <64745304+davidegelja@users.noreply.github.com>
2021-12-04 17:26:11 +0100

    CottageOwner merge

---
+++ src/main/java/stasaaleksadavid/isabackend/model/CottageFreeAppointment.java
@@ -0,0 +1,88 @@
+package stasaaleksadavid.isabackend.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Cottage_Free_Appointments")
+public class CottageFreeAppointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "Starting_Date")
+    private LocalDate startingDate;
+
+    @Column(name = "Ending_Date")
+    private LocalDate endingDate;
+
+    @Column(name = "Numer_Of_People")
+    private int numberOfPeople ;
+
+    @Column(name = "Additional_Services")
+    private String additionalServices;
+
+    @Column(name = "Price")
+    private double price;
+
+    public CottageFreeAppointment() {
+    }
+
+    public CottageFreeAppointment(LocalDate startingDate, LocalDate endingDate, int numberOfPeople, String additionalServices, double price) {
+        super();
+        this.startingDate = startingDate;
+        this.endingDate = endingDate;
+        this.numberOfPeople = numberOfPeople;
+        this.additionalServices = additionalServices;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(LocalDate startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public LocalDate getEndingDate() {
+        return endingDate;
+    }
+
+    public void setEndingDate(LocalDate endingDate) {
+        this.endingDate = endingDate;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public String getAdditionalServices() {
+        return additionalServices;
+    }
+
+    public void setAdditionalServices(String additionalServices) {
+        this.additionalServices = additionalServices;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
