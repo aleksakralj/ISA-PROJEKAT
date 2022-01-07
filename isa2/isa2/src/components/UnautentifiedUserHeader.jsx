a820c7eecf5a043195108bdb4d47c8cf22aa6b0d
aleksakralj <64970957+aleksakralj@users.noreply.github.com>
2022-01-07 16:24:41 +0100

    Different Headers for different roles
    - feature - different headers for different roles (finished for client and unautentified user)

---
+++ isa2/isa2/src/components/UnautentifiedUserHeader.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+const navLinkStyle = {
+    color: 'black'
+}
+
+class Unautentifieduserheader extends Component {
+    render() {
+        return (
+           
+                 <div>
+                <Navbar>
+                    <Container>
+                        <img src="https://cdn-icons.flaticon.com/png/512/2970/premium/2970068.png?token=exp=1641505178~hmac=b8a6dc0406e9945c6209f8071a7174c2" alt="" width="80px" height="60px" />
+                        <Navbar.Brand style={navLinkStyle} href="/homepage">FISHING BOOKER</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">                            
+                            <Nav>
+                                <Nav.Link style={navLinkStyle} href="/login">Log in</Nav.Link>
+                                <Nav.Link style={navLinkStyle} eventKey={2} href="registeruser">
+                                    Register account
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div> 
+           
+        );
+    }
+}
+
+export default Unautentifieduserheader;
