24d4db80ab5780548e23c645ea8d69330a4cb3be
aleksakralj <64970957+aleksakralj@users.noreply.github.com>
2022-05-30 16:29:32 +0200

    Feature subscriptions for user
    + front end (finished)
    + back end (finished)

--- isa2/isa2/src/Conponents/EntitySubscriptionLane.jsx
+++ isa2/isa2/src/Conponents/EntitySubscriptionLane.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import '../Assets/Styles/SubscriptionNavigatorPage.css'
 import { useState, useEffect } from 'react';
+import EntitySubscriptionsAPI from '../services/EntitySubscriptionsAPI';
+
 
 const EntitySubscriptionLane = ({passData, flag, imageAddress}) => {
 
-    const [adventures, setadventures] = useState([]);
-    const [cottages, setcottages] = useState([]);
-    const [ships, setships] = useState([]);
+    const [activeUser, setActiveUser] = useState({});
 
-    const loadData = () => {
+    useEffect(() => {
+        getUser();
+    }, [])
 
-        if(flag === 'Adventures'){
-            setadventures(passData);
+    const getUser = () => {
+        let user = JSON.parse(localStorage.getItem('activeUser'));
+        setActiveUser(user);
+    }
+
+    const unsub = (id) => {
+
+        if(flag === 'Adventures') {
+            EntitySubscriptionsAPI.deleteAdventureSubscriptionByUserIdAndAdventureId(activeUser.id, id);
+            window.location.reload(false);            
+        }
+        
+        else if( flag === 'Ships') {
+            EntitySubscriptionsAPI.deleteShipSubscriptionByUserIdAndShipId(activeUser.id, id )
+            window.location.reload(false);
         }
 
-        else if(flag === 'Cottages') {
-            setcottages(passData)
-        }
-
-        else if(flag === 'Ships') {
-            setships(passData)
+        else if( flag === 'Cottages') {
+            EntitySubscriptionsAPI.deleteCottageSubscriptionByUserIdAndCottageId(activeUser.id, id)
+            window.location.reload(false);
         }
 
     }
-
-    useEffect(()=> {
-        loadData();    
-    },[])
 
     return (
         <div className='subscription-lane'>
@@ -34,12 +42,23 @@ const EntitySubscriptionLane = ({passData, flag, imageAddress}) => {
                 <h4 className='caption'>{flag} Subs:</h4>
                 <img className='picture' src={imageAddress}></img>
             </div>
-            <div className='subscription-card'>
-                <label>
-                    {passData.name}
-                </label>
-                <button>Unsub</button>
-            </div>
+            <ul className='subscription-list'>
+                {
+                passData.map((data) => {
+                    const {
+                        id,
+                        name
+                    } = data;
+                    return (
+                        <div className='subscription-card' key={id}>
+                            <label>
+                                {name}
+                            </label>
+                            <button onClick={() => unsub(id)}>Unsub</button>
+                        </div>
+                    )})
+                }
+            </ul>
         </div>
     );
 }
